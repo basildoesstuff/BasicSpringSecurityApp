e14c723e3ac34ff96574b6eda45741c4239143e9
basildoesstuff <contact.basilsunny@gmail.com>
2024-10-17 14:17:31 +0530

    init commit

---
+++ src/main/java/com/example/demo/config/MySecurityConfig.java
@@ -0,0 +1,41 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class MySecurityConfig {
+
+    //using depricated http.httpBasic() &   http.authorizeHttpRequests()
+    // after 3.2.x http.httpBasic() passes an argument of type of authentication used
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.httpBasic();
+        http.authorizeHttpRequests().anyRequest().authenticated();
+        return http.build();
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService()
+    {
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+        UserDetails user = User.withUsername("tom").password(passwordEncoder().encode("helloworld"))
+                .authorities("read").build();
+        userDetailsService.createUser(user);
+        return userDetailsService ;
+    }
+
+}
